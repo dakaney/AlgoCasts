83f4587d4ab8f11a3e2065864691a225af14f812
Dakaney <wayman_ng@yahoo.com>
2018-12-29 22:43:17 -0800

    completed reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,22 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let arr = JSON.stringify(n).split('');
+    let neg = false;
+    let result = '';
+    if (arr[0] === '-') {
+      neg = true;
+      arr.shift();
+    }
+    for (let i of arr) {
+      result = i + result;
+    }
+    result = Number(result);
+    if (neg) {
+      result *= -1;
+    }
+    return result;
+  }
 
 module.exports = reverseInt;
