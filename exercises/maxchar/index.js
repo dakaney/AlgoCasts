5ea3c4335b7b835d1e201a8e67a94cf6ec4527e6
Dakaney <wayman_ng@yahoo.com>
2018-12-30 00:03:59 -0800

    completed maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let storage = {};
+    let highestCount = 0;
+    let highestChar = str[0];
+    for (let char of str) {
+        storage[char] = storage[char] + 1 || 1;
+        if (highestCount < storage[char]) {
+            highestCount = storage[char];
+            highestChar = char;
+        }
+    };
+    return highestChar;
+}
 
 module.exports = maxChar;
