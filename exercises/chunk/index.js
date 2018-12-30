69a3e6ab5b9a977dc11a77b8260ad9b8b97ea05a
Dakaney <wayman_ng@yahoo.com>
2018-12-30 11:02:59 -0800

    completed chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,14 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let i = 0;
+    let res = [];
+    while (i < array.length) {
+        res.push(array.slice(i, i + size));
+        i += size;
+    }
+    return res;
+}
 
 module.exports = chunk;
